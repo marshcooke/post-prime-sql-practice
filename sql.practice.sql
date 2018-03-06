7b0e91ccd73bdb2a3fbe0c71978b75487d98b3e0
Sarah Cooke <marshcooke1@gmail.com>
2018-03-06 00:17:39 -0600

    completed multiple tables - part 4 of sql tutorial

--- sql.practice.sql
+++ sql.practice.sql
@@ -195,5 +195,7 @@ iv. MULTIPLE TABLES
         SELECT * FROM newspaper UNION SELECT * FROM online;
 
     9. WITH
+        WITH previous_query AS (SELECT customer_id, COUNT (subscription_id) as subscriptions FROM orders GROUP BY customer_id)
+        SELECT customers.customer_name, previous_query.subscriptions FROM previous_query JOIN customers ON customers.customer_id = previous_query.customer_id;
 
     10. REVIEW
